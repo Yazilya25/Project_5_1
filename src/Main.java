2d29c819bd294f9fc6446caa4709afddcb0a70db
Абдуллина Язиля <A_250494@mail.ru>
2022-05-10 22:27:02 +0300

    Удаление лишних файлов

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BmiService service = new BmiService();
-        float weight = 52;
-        float height = (float) 1.65;
-        float bmi = service.calculate(weight, height);
-        System.out.println("Ваш Индекс массы тела: " + bmi);
-    }
-}
